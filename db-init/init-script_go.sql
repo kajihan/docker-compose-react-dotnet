be07407620576fb7fbb79f27040306b473e3f4f6
Artur Burov <burov.arthur@gmail.com>
2024-07-30 16:59:42 +0300

    added healthcheck as sql script runner

---
+++ db-init/init-script_go.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE Products;
+GO
+
+USE Products;
+GO
+
+CREATE TABLE Products (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100),
+    Price DECIMAL(10, 2)
+);
+GO
+
+INSERT INTO Products (Name, Price)
+  VALUES ('Sugar',  32.0),
+         ('Sault', 19.0),
+         ('Bread', 20.0),
+         ('Butter', 62.0),
+         ('Milk', 32.0);
+GO
